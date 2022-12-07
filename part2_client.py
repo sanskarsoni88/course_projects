3ccff0b3fb1fce447ec15d991004a13339233d1a
sanskarsoni88 <sanskar.soni88@gmail.com>
2022-12-07 13:56:47 -0800

    Update part2_client.py

--- part2_client.py
+++ part2_client.py
@@ -13,16 +13,16 @@ for i in range(5):     #5 messages in total
     message = f"PING {i} - hello world"
     clientSocket.sendto(message.encode(), (serverName, serverPort))   #send message to server
 
-    sendTime = time.time()     #sendTime is the time at which message is sent to server
-    clientSocket.settimeout(1) # Set the maximum delay to be 1 second
+    sendTime = time.time()        #sendTime is the time at which message is sent to server
+    clientSocket.settimeout(1)    # Set the maximum delay to be 1 second
 
-    try:      #try recieving the message from the server in under a second
+    try:                          #try recieving the message from the server in under a second
         modifiedMessage, serverAddress = clientSocket.recvfrom(2048)  #message from server recieved
         receiveTime = time.time()                                     #time at which message is recieved recorded
 
-        RTT = 1000 * round(receiveTime - sendTime, 3)     #calculate round trip time in seconds (round to 3 decimals) and convert to ms finally
-        print(f'{RTT} ms')                                #print RTT in ms
-        print(modifiedMessage.decode())                   #print message recieved from server
+        RTT = 1000 * round(receiveTime - sendTime, 3)                 #calculate round trip time in seconds (round to 3 decimals) and convert to ms finally
+        print(f'{RTT} ms')                                            #print RTT in ms
+        print(modifiedMessage.decode())                               #print message recieved from server
 
     except timeout:     #message was not recieved from server in under a second
         print('request timed out')
